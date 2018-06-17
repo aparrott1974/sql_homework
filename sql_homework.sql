c89c5c052bf41d1ae5ac9bc8e4bb3e4b5d93d73e
Andrew Parrott <aparrott@blizzard.com>
2018-06-17 17:37:21 -0500

    update

--- sql_homework.sql
+++ sql_homework.sql
@@ -137,6 +137,7 @@ where actor_id = 172
 NOTE:
 i need to figure out what's going on after phone 
 */
+
 use sakila;
 CREATE TABLE address (
   address_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -205,70 +206,246 @@ group by
 * 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
 */
 
-
+select 
+	f.title,
+    count(*)
+from sakila.film f
+	inner join sakila.inventory i
+		on f.film_id = i.film_id
+where f.title = 'hunchback impossible'
+group by
+	f.title
+;
 
 /*
-* 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
-
-  ```
-  	![Total amount paid](Images/total_payment.png)
-  ```
+* 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. 
+List the customers alphabetically by last name:
 */
 
+select 
+	c.first_name,
+    c.last_name,
+    sum(p.amount) as 'total paid'
+from sakila.customer c
+	left join sakila.payment p
+		on c.customer_id = p.customer_id
+group by 
+	c.first_name,
+    c.last_name
+order by
+	c.last_name
+        
+;
 
 /*
-* 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English.
+* 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
+As an unintended consequence, films starting with the letters `K` and `Q` have also soared in popularity. 
+Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English.
 */
 
+select 
+	f.title,
+    lg.language
+from sakila.film f 
+	inner join 
+		(
+		select 
+			l.language_id,
+            l.name as language
+        from sakila.language l
+        where l.name = 'english'
+        ) lg
+        on f.language_id = lg.language_id
+where f.title like 'k%'
+	or f.title like 'q%'
+;
 
 /*
 * 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
 */
 
+select 
+	fd.title,
+    a.first_name,
+    a.last_name
+from sakila.actor a
+	inner join
+	(
+	select 
+		fa.actor_id,
+		fi.film_id,
+		fi.title
+	from sakila.film_actor fa
+		inner join 
+		(
+		select 
+			f.film_id,
+			f.title
+		from sakila.film f
+		where f.title = 'alone trip'
+		) fi
+			on fa.film_id = fi.film_id
+	)fd
+		on a.actor_id = fd.actor_id
+;
 
 /*
-* 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+* 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses 
+of all Canadian customers. Use joins to retrieve this information.
 */
 
+select 
+	co.first_name,
+    co.last_name,
+    co.email,
+    cn.country
+from sakila.country cn
+	inner join sakila.city ct
+		on cn.country_id = ct.country_id
+	inner join sakila.address ad
+		on ct.city_id = ad.city_id
+	inner join sakila.customer co 
+		on ad.address_id = co.address_id
+where cn.country = 'canada'
+
+;
 
 /*
-* 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
+* 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+Identify all movies categorized as famiy films.
 */
 
+select 
+	f.title,
+    ct.name
+from sakila.category ct
+	inner join sakila.film_category fc
+		on ct.category_id = fc.category_id
+	inner join sakila.film f
+		on fc.film_id = f.film_id
+where ct.name = 'family'
+
+;
 
 /*
 * 7e. Display the most frequently rented movies in descending order.
 */
 
 
+select 
+	f.title,
+    count(r.rental_id) as rental_count
+from sakila.rental r
+	inner join sakila.inventory i
+		on r.inventory_id = i.inventory_id
+	inner join sakila.film f
+		on i.film_id = f.film_id
+group by
+	f.title
+order by 
+	count(r.rental_id) desc
+;
+
+
 /*
 * 7f. Write a query to display how much business, in dollars, each store brought in.
 */
 
+select 
+	s.store_id,
+    ad.address,
+    concat('$', format(sum(p.amount), 2)) as revenue
+from sakila.store s
+	inner join sakila.address ad
+		on s.address_id = ad.address_id
+	inner join sakila.staff st
+		on s.store_id = st.store_id
+	inner join sakila.payment p
+		on st.staff_id = p.staff_id
+group by
+	s.store_id,
+    ad.address
+;
 
 /*
 * 7g. Write a query to display for each store its store ID, city, and country.
 */
 
-
+select 
+	s.store_id,
+    cy.city,
+    cn.country
+from sakila.store s
+	inner join sakila.address ad
+		on s.address_id = ad.address_id
+	inner join sakila.city cy
+		on ad.city_id = cy.city_id
+	inner join sakila.country cn
+		on cy.country_id = cn.country_id
+;
 /*
-* 7h. List the top five genres in gross revenue in descending order. (**Hint**: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+* 7h. List the top five genres in gross revenue in descending order. 
+(**Hint**: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
 */
 
+select 
+	ct.name,
+    sum(pm.amount) as revenue
+from sakila.category ct
+	inner join sakila.film_category fc
+		on ct.category_id = fc.category_id
+	inner join sakila.inventory i 
+		on fc.film_id = i.film_id
+	inner join sakila.rental r
+		on i.inventory_id = r.inventory_id
+	inner join sakila.payment pm
+		on r.rental_id = pm.rental_id
+group by
+	ct.name
+order by sum(pm.amount) desc
+
+;
+
 
 /*
-* 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+* 8a. In your new role as an executive, you would like to have an easy way of viewing the 
+Top five genres by gross revenue. Use the solution from the problem above to create a view. 
+If you haven't solved 7h, you can substitute another query to create a view.
 */
 
+create or replace view top_five_genres as 
+select 
+	ct.name,
+    sum(pm.amount) as revenue
+from sakila.category ct
+	inner join sakila.film_category fc
+		on ct.category_id = fc.category_id
+	inner join sakila.inventory i 
+		on fc.film_id = i.film_id
+	inner join sakila.rental r
+		on i.inventory_id = r.inventory_id
+	inner join sakila.payment pm
+		on r.rental_id = pm.rental_id
+group by
+	ct.name
+order by sum(pm.amount) desc
+limit 5
+;
 
 /*
 * 8b. How would you display the view that you created in 8a?
 */
 
+select *
+from top_five_genres;
 
 /*
 * 8c. You find that you no longer need the view `top_five_genres`. Write a query to delete it.
+*/
 
+drop view top_five_genres;
+
+/*
 ## Appendix: List of Tables in the Sakila DB
 */
 
